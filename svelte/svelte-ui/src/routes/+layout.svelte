d8e3601ef71e1cd0faf4ce23d805cb0f13138278
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-04 13:43:28 -0800

    Update +layout.svelte

--- svelte/svelte-ui/src/routes/+layout.svelte
+++ svelte/svelte-ui/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
+<script>
+  import favicon from '$lib/assets/favicon.png';
+</script>
+
 <svelte:head>
-	<link rel="icon" href="/projectbaguette/assets/favicon.png" />
+  <link rel="icon" href={favicon} />
+  <title>Project Baguette</title>
 </svelte:head>
 
 <slot />

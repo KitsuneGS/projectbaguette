358aeaf5e6ef5ff03cf40a6de96f354b34546bc1
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-04 11:52:42 -0800

    Update svelte.config.js

--- svelte/svelte-ui/svelte.config.js
+++ svelte/svelte-ui/svelte.config.js
@@ -1,21 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-	paths: {
-    base: process.env.NODE_ENV === 'production' ? '/projectbaguette' : ''
-}
-		prerender: {
-			handleHttpError: 'ignore'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    })
+  }
 };
 
 export default config;

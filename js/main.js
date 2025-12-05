198992236f10150e83222059cfea84e7ee313a88
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 09:30:44 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-export {};
+// js/main.js
+import { BASE } from "/js/base.js";
 
-// Import submodules normally:
-import "./game/core.js";
-import "./game/audio.js";
-import "./game/timing.js";
-import "./game/notes.js";
-import "./game/input.js";
-import "./game/judge.js";
-import "./game/particles.js";
-import "./game/render.js";
-import "./game/loop.js";
-import "./game/start.js";
+// Load modules relative to BASE
+await import(`${BASE}/js/game/core.js`);
+await import(`${BASE}/js/game/time.js`);
+await import(`${BASE}/js/game/audio.js`);
+await import(`${BASE}/js/game/notes.js`);
+await import(`${BASE}/js/game/input.js`);
+await import(`${BASE}/js/game/judge.js`);
+await import(`${BASE}/js/game/particles.js`);
+await import(`${BASE}/js/game/render.js`);
+await import(`${BASE}/js/game/loop.js`);
+await import(`${BASE}/js/game/start.js`);

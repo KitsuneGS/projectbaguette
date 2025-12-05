b51f8056d55ff6263d80138cd56ad7b6d20850c3
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 08:25:59 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// js/main.js
-// Just wiring up the engine side effects
+// js/main.js  (no changes needed if this matches your structure)
 import "./game/core.js";
 import "./game/audio.js";
+import "./game/chart.js";     // if you split charting
 import "./game/notes.js";
-import "./game/input.js";
+import "./game/judge.js";
 import "./game/render.js";
 import "./game/loop.js";
 import "./game/start.js";
-import "./game/timing.js";

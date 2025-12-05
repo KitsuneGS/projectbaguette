0eeb130a7020aa48c9bfd2669d23b17f28a269ae
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 08:59:11 -0800

    Delete js/game/time.js

--- js/game/time.js
+++
@@ -1,7 +0,0 @@
-// js/game/time.js
-//not to be confused with timING.js
-export function getSongTime() {
-  const audio = document.getElementById("song");
-  if (audio && !isNaN(audio.currentTime)) return audio.currentTime;
-  return 0;
-}

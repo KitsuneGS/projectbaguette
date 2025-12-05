037ecb2c181799043e90e2b3235cdfa226f042cd
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 08:14:57 -0800

    Create judge.js

---
+++ js/game/judge.js
@@ -0,0 +1,48 @@
+// js/game/judge.js
+// Hit/Miss rules + score/combo state
+
+import { getSongTime } from "./time.js";
+import { LANES } from "./lanes.js";
+import { addParticles } from "./particles.js";
+
+export let score = 0;
+export let combo = 0;
+export let lastHitText = "";
+export let lastHitTime = 0;
+
+export const HIT_WINDOW_PERFECT = 0.08;
+export const HIT_WINDOW_GOOD = 0.18;
+export const MISS_EXTRA = 0.20;
+
+// Called from input or auto miss
+export function judge(n, t) {
+  const d = Math.abs(n.time - t);
+
+  if (d <= HIT_WINDOW_PERFECT) registerHit(n, "COOL", 300);
+  else if (d <= HIT_WINDOW_GOOD) registerHit(n, "FINE", 100);
+  else if (d <= 0.35) registerMiss(n);
+}
+
+export function registerHit(n, label, baseScore) {
+  n.judged = true;
+  n.effect = "hit";
+  n.effectTime = getSongTime();
+
+  combo++;
+  score += Math.floor(baseScore * (1 + combo * 0.05));
+
+  lastHitText = label;
+  lastHitTime = getSongTime();
+
+  addParticles(n.targetX, n.targetY, LANES[n.lane].color);
+}
+
+export function registerMiss(n) {
+  n.judged = true;
+  n.effect = "miss";
+  n.effectTime = getSongTime();
+
+  combo = 0;
+  lastHitText = "MISS";
+  lastHitTime = getSongTime();
+}

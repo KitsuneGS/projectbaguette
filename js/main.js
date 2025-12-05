593c5291943f4bcc9fbb6a6ee801288cc426a911
coding hacker guy <132928035+therealsegfault@users.noreply.github.com>
2025-12-05 08:53:46 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // js/main.js  (no changes needed if this matches your structure)
 import "./game/core.js";
 import "./game/audio.js";
-import "./game/chart.js";     // if you split charting
 import "./game/notes.js";
 import "./game/judge.js";
 import "./game/render.js";
